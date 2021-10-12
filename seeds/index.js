1204da563c134f6976c0add3c3ff5f86499fe493
Jibbyworks <JDBucog@yahoo.com>
2021-10-12 14:34:03 -0700

    add morgan middleware

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const mongoose = require("mongoose")
+const cities = require("./cities")
+const { places, descriptors } = require("./seedHelpers")
+const Campground = require("../models/campground")
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
 
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error: "))
 db.once("open", () => {
-    console.log("Database connected");
-});
+  console.log("Database connected")
+})
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
-
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
+const randomizeName = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)]
 }
 
-seedDB().then(() => {
-    mongoose.connection.close();
-})
+const seedDB = async () => {
+  await Campground.deleteMany({})
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000)
+    const randomPlace = Math.floor(Math.random() * places.length)
+    const randomDescriptor = Math.floor(Math.random() * descriptors.length)
+    const newCamp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${places[randomPlace]} ${descriptors[randomDescriptor]}`,
+    })
+    await newCamp.save()
+  }
+}
+
+seedDB().then(() => db.close())
