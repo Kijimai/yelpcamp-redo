8dba07d3e20e4c57c4985132904632f3ea51f4bb
Jibbyworks <JDBucog@yahoo.com>
2021-10-29 14:31:49 -0700

    Add popup, and popup styles

--- public/scripts/showMap.js
+++ public/scripts/showMap.js
@@ -3,7 +3,15 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
   center: coords, // starting position [lng, lat]
-  zoom: 11, // starting zoom
+  zoom: 9, // starting zoom
 })
 
-new mapboxgl.Marker().setLngLat(coords).addTo(map)
+// const popup = new mapboxgl.Popup({ closeOnClick: false })
+//   .setLngLat(coords)
+//   .setHTML("<h2>Hello World!</h2>")
+//   .addTo(map)
+
+new mapboxgl.Marker()
+  .setLngLat(coords)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML("<h2>Hello world!</h2>"))
+  .addTo(map)
