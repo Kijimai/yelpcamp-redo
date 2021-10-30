676f57eb330756f9317b11e207e5db8c631df2cf
Jibbyworks <JDBucog@yahoo.com>
2021-10-29 20:39:07 -0700

    add cluster map

--- public/scripts/showMap.js
+++ public/scripts/showMap.js
@@ -1,17 +1,12 @@
 mapboxgl.accessToken = mapToken
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/dark-v10", // style URL
   center: coords, // starting position [lng, lat]
   zoom: 9, // starting zoom
 })
 
-// const popup = new mapboxgl.Popup({ closeOnClick: false })
-//   .setLngLat(coords)
-//   .setHTML("<h2>Hello World!</h2>")
-//   .addTo(map)
-
 new mapboxgl.Marker()
   .setLngLat(coords)
-  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML("<h2>Hello world!</h2>"))
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${title}</h3>`))
   .addTo(map)
