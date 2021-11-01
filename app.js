fe773780f25dd00e9c03e40bb61f4c969540715c
Jibbyworks <JDBucog@yahoo.com>
2021-11-01 15:04:46 -0700

    install helmet. modified content security policy to allow only specified sites and other resources. Check the codebase.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const campgroundsRouter = require("./routes/campgrounds")
 const reviewsRouter = require("./routes/reviews")
 const usersRouter = require("./routes/users")
 const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require("helmet")
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -50,6 +51,8 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(mongoSanitize())
 
 const sessionConfig = {
+  //rename default session cookie name
+  name: "session-cookie",
   //Temporary secret
   secret: "thisismysecret",
   resave: false,
@@ -59,11 +62,59 @@ const sessionConfig = {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
+    //Secure: true - forces cookies to work ONLY through https://, not through localhost
+    // secure: true,
   },
 }
 
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet())
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com",
+  "https://api.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://kit.fontawesome.com",
+  "https://cdnjs.cloudflare.com",
+  "https://cdn.jsdelivr.net",
+]
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com",
+  "https://stackpath.bootstrapcdn.com",
+  "https://cdn.jsdelivr.net",
+  "https://api.mapbox.com",
+  "https://api.tiles.mapbox.com",
+  "https://fonts.googleapis.com",
+  "https://use.fontawesome.com",
+]
+const connectSrcUrls = [
+  "https://api.mapbox.com",
+  "https://*.tiles.mapbox.com",
+  "https://events.mapbox.com",
+]
+const fontSrcUrls = ["https://fonts.gstatic.com"]
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      childSrc: ["blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://images.unsplash.com",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+)
 
 app.use(passport.initialize())
 
